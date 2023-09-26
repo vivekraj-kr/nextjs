668b11ada55c166ac64e8d9659f2898f806aad35
Vivekraj K R <vivekraj.kr@smartr365.com>
2023-09-26 17:48:23 +0530

    Fetch the data from strapi

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,29 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
   return (
     <main className={styles.main}>
-      <h1>Hello Next js</h1>
+      <h1>Hello next js</h1>
+      {data?.map((user) => (
+        <p>
+          {user?.username} - {user?.email}
+        </p>
+      ))}
     </main>
   );
+}
+
+async function getData() {
+  const res = await fetch("https://strapi-be-lsoi.onrender.com/api/users");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
 }
